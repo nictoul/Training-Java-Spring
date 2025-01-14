abb1c92511329d494d9d8b7c0e0b504558488029
Nicolas Toulouse <nicolastoulouse2@hotmail.com>
2025-01-14 07:29:33 -0500

    Improve Rest controller

--- src/main/java/training/java/REST/SimpleRestController.java
+++ src/main/java/training/java/REST/SimpleRestController.java
@@ -1,29 +1,27 @@
 package training.java.REST;
 
-import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-public class RestController {
-    private static final Logger logger = LoggerFactory.getLogger(RestController.class);
+@RestController
+public class SimpleRestController {
+    private static final Logger logger = LoggerFactory.getLogger(SimpleRestController.class);
 
-    @RequestMapping("/get") //specify to serve the base path. ex: localhost:8080
+    @GetMapping("/get") //specify to serve the base path. ex: localhost:8080
     public ResponseEntity home(){
         logger.info("RestController get route called");
         return ResponseEntity.ok("Hello world! You can rest if you are tire but you should try REST API instead");
     }
 
+    //    curl --location 'http://localhost:8080/post?paramExample=this%20parameter%20is%20mandatory' --header 'Content-Type: application/json' --data '{"id": 123,"name": "toto"}'
     @PostMapping("/post")
     public ResponseEntity showPostExample(@RequestParam String paramExample, @RequestParam(required = false) String optional, @RequestBody RestObject restObject){
         logger.info("RestController post route called");
         logger.info("paramExample: " + paramExample);
         logger.info("optional: " + optional);
-        logger.info("SimpleObject: " + restObject);
+        logger.info("RestObject: " + restObject);
 
         return ResponseEntity.ok("");
     }
